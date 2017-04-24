a880c57e6e5810dda7a1f9bd6641264a83a83ac4
Rene Hexel <r.hexel@griffith.edu.au>
2017-04-25 06:32:09 +1000

    Add build infrastructure

--- Package.swift
+++ Package.swift
@@ -3,5 +3,8 @@
 import PackageDescription
 
 let package = Package(
-    name: "GtkListBoxDNDDemo"
+    name: "GtkListBoxDNDDemo",
+    dependencies: [
+        .Package(url: "https://github.com/rhx/SwiftGtk.git", majorVersion: 3)
+    ]
 )
