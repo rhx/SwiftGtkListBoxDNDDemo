be83b413d479429131b3c892994d50da0a459d6d
Rene Hexel <r.hexel@griffith.edu.au>
2022-04-30 11:31:42 +1000

    Use monorepo SwiftGtk

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "GtkListBoxDNDDemo",
     dependencies: [
-        .package(url: "https://github.com/rhx/SwiftGtk.git", branch: "gtk3"),
+        .package(url: "https://github.com/rhx/SwiftGtk.git", branch: "monorepo"),
     ],
     targets: [
         .target(name: "GtkListBoxDNDDemo", dependencies: [
