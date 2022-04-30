a2aaf8e736eee848d27ef1c92715f1b110efe291
Rene Hexel <r.hexel@griffith.edu.au>
2022-04-30 11:30:53 +1000

    Upgrade to Swift 5.6 and use plugin

--- Package.swift
+++ Package.swift
@@ -1,14 +1,15 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.6
 
 import PackageDescription
 
 let package = Package(
     name: "GtkListBoxDNDDemo",
     dependencies: [
-        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("main")),
-        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("gtk3")),
+        .package(url: "https://github.com/rhx/SwiftGtk.git", branch: "gtk3"),
     ],
     targets: [
-        .target(name: "GtkListBoxDNDDemo", dependencies: ["Gtk"]),
+        .target(name: "GtkListBoxDNDDemo", dependencies: [
+            .product(name: "Gtk", package: "SwiftGtk"),
+        ]),
     ]
 )
