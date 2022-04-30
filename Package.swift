4d4f6bc21d72d03a8e24b69942f249762d9574e1
Rene Hexel <r.hexel@griffith.edu.au>
2022-04-30 11:27:30 +1000

    Swift 5.2 branch

--- Package.swift
+++ Package.swift
@@ -5,8 +5,8 @@ import PackageDescription
 let package = Package(
     name: "GtkListBoxDNDDemo",
     dependencies: [
-        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("main")),
-        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("gtk3")),
+        .package(name: "gir2swift", url: "https://github.com/rhx/gir2swift.git", .branch("swift52")),
+        .package(name: "Gtk", url: "https://github.com/rhx/SwiftGtk.git", .branch("swift52")),
     ],
     targets: [
         .target(name: "GtkListBoxDNDDemo", dependencies: ["Gtk"]),
