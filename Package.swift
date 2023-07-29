23a8220b9d051b5e6ad06a8b663822be152d5a23
Rene Hexel <r.hexel@griffith.edu.au>
2023-07-29 17:05:59 +1000

    Depend on development branch

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "GtkListBoxDNDDemo",
     dependencies: [
-        .package(url: "https://github.com/rhx/SwiftGtk.git", branch: "monorepo"),
+        .package(url: "https://github.com/rhx/SwiftGtk.git", branch: "monorepo-development"),
     ],
     targets: [
         .executableTarget(name: "GtkListBoxDNDDemo", dependencies: [
